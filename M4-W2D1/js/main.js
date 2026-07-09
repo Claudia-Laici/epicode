bf2303c86057d70d6f6f845f7998271bd6feb45a
Claudia <laiciclaudia@gmail.com>
2026-07-09 13:27:02 +0200

    svolto esercizio di pexels, dove utilizziamo la fetch per ricreare delle card, e aggiunto la ricerca immagini

---
+++ M4-W2D1/js/main.js
@@ -0,0 +1,43 @@
+const gallery = document.getElementById("gallery")
+const searchInput = document.getElementById("searchInput")
+const searchBtn = document.getElementById("searchBtn")
+
+const searchImages = (query) => {
+  return fetch(`https://api.pexels.com/v1/search?query=${query}`, {
+    headers: {
+      Authorization: "npRmeA7F0tKIBfFqLfwZAcCq0T2hY6VxRxlfUkOkitLhLn7ts7oeWAou",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      data.photos.forEach((photo) => {
+        const card = document.createElement("div")
+
+        card.className = "col-md-2 pb-5"
+
+        card.innerHTML = `  <div class="card h-100">
+        <img src="${photo.src.medium}" class="card-img-top " alt="${photo.alt}">
+        <div class = "card-body">
+            <h5 class = "card-title">${photo.photographer}</h5>
+            <p class = "card-text">${photo.alt}</p>
+        </div>
+    </div> 
+    `
+        gallery.appendChild(card)
+      })
+    })
+
+    .catch((e) => console.log(e))
+}
+
+searchBtn.addEventListener("click", () =>{
+    const query = searchInput.value.trim()
+
+    if (query === "") return
+
+    gallery.innerHTML = ""
+
+    searchImages(query)
+})
+
+
